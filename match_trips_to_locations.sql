1c5421b4a6a45f06fabfbf2d9c74ab58ab2c5203
Raghu Dhara <rdhara@users.noreply.github.com>
2022-05-09 20:08:20 -0700

    Update match_trips_to_locations.sql

--- match_trips_to_locations.sql
+++ match_trips_to_locations.sql
@@ -3,6 +3,8 @@ select
   current_timestamp experiment_time,
   citibikes.avg_distance,
   citibikes.avg_duration
-from citibikes
-join starbucks
-using (lat, lon)
+from citibikes c
+join starbucks s
+where 1=1
+  and round(c.lat, 3) = round(s.lat, 3)
+  and round(c.lon, 3) = round(c.lon, 3)
