da2084d9ce1da2b6b97de56b327c8e9ba2237c3b
Demo Account <demo@kaspian.io>
2022-10-07 09:23:19 -0700

    test

--- match_trips_to_locations.sql
+++ match_trips_to_locations.sql
@@ -1,1 +1,10 @@
-[object Object]
+select
+  s.store_name,
+  current_timestamp experiment_time,
+  c.avg_distance,
+  c.avg_duration
+from citibikes c
+join starbucks s
+where 1=1
+  and round(c.lat, 2) = round(s.lat, 2)
+  and round(c.lon, 2) = round(s.lon, 2)
